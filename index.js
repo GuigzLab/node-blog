150cd3799be2c4608cd2d09743cf59a1da55b056
GuigzLab <guillaume.dufau.pro@gmail.com>
2020-03-12 15:30:26 +0100

    Connexion à la base de données Atlas

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -13,6 +16,13 @@ const HOST = 'localhost';
 
 app.use('/', blogRouter);
 
-app.listen(PORT, HOST, () => {
-     console.log(`Listening on http://${HOST}:${PORT}`);
-});
+const uri = `mongodb+srv://${encodeURIComponent(process.env.DB_USER)}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => console.log('[MongoDB] Connected'))
+     .then( () => {
+          app.listen(PORT, HOST, () => {
+               console.log(`[Express] Listening on http://${HOST}:${PORT}`);
+          });
+     })
+     .catch(err => console.error(err))
